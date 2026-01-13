9a4c0f4665d470ae4fccc7717288d384f3400d64
francescopausellii <francescopauselli03@gmail.com>
2026-01-13 12:35:21 +0100

    fix: changed useState logic

--- src/components/toggle-data.jsx
+++ src/components/toggle-data.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
 import { Label } from "./ui/label";
 import { Switch } from "./ui/switch";
 
 export default function ToggleData({ onChange }) {
-  const [, setShowData] = useState(false);
-
-  const handleToggle = (checked) => {
-    setShowData(checked);
-    onChange?.(checked);
-  };
-
   return (
     <div className="flex gap-2.5">
       <Label htmlFor="show-data">Mostra Dati</Label>
       <Switch
         id="show-data"
-        onCheckedChange={(checked) => handleToggle(checked)}
+        onCheckedChange={(checked) => onChange?.(checked)}
       />
     </div>
   );
